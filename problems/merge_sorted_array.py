59e67e2e733ed4a73c5bf2ee6be9e859033448ca
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-10-07 15:37:09 +0300

    found a testcase that is failed

--- problems/merge_sorted_array.py
+++ problems/merge_sorted_array.py
@@ -17,7 +17,7 @@ Example 1:
 
 Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 >>> Solution().merge([1,2,3,0,0,0], 3,[2,5,6],3)
-[1,2,2,3,5,6]
+[1, 2, 2, 3, 5, 6]
 
 Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
 The result of the merge is [1,2,2,3,5,6] with the underlined elements coming
@@ -40,6 +40,10 @@ Explanation: The arrays we are merging are [] and [1].
 The result of the merge is [1].
 Note that because m = 0, there are no elements in nums1. The 0 is only there to
  ensure the merge result can fit in nums1.
+
+>>> Solution().merge([-1,0,0,3,3,3,0,0,0],6,[1,2,2],3)
+[-1,0,0,1,2,2,3,3,3]
+
 """
 from typing import List
 import doctest
