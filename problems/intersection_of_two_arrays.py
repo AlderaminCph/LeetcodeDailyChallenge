0dc7dc7095313088c8fdcc54b72743335b68757c
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-10-08 21:18:15 +0300

    doctests were written

--- problems/intersection_of_two_arrays.py
+++ problems/intersection_of_two_arrays.py
@@ -7,18 +7,20 @@ in both arrays and you may return the result in any order.
 
 Example 1:
 
-Input: nums1 = [1,2,2,1], nums2 = [2,2]
-Output: [2,2]
+>>> Solution().intersect([1,2,2,1], [2,2])
+[2,2]
 
 Example 2:
 
-Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-Output: [4,9]
+>>> Solution().intersect([4,9,5], [9,4,9,8,4])
+[4,9]
+
 Explanation: [9,4] is also accepted.
 
 """
 from typing import List
 from collections import defaultdict
+import doctest
 
 
 class Solution:
@@ -38,3 +40,6 @@ class Solution:
             [i for i in list(hashtable.values()) if i >= 1]
         ]  # noqa
         return res
+
+
+doctest.testmod()
