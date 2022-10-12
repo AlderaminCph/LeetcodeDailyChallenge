772cf43eca8c998ab24db9368aaf55931349029c
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-10-13 00:04:32 +0300

    more testcases were added

--- problems/largest_perimeter_triangle.py
+++ problems/largest_perimeter_triangle.py
@@ -14,6 +14,12 @@ Example 2:
 
 >>> largestPerimeter([1,2,1])
 0
+
+>>> largestPerimeter([1,2,2,4,18,8])
+5
+
+>>> largestPerimeter([3,2,3,4])
+10
 """
 from typing import List
 import doctest
@@ -22,7 +28,7 @@ import doctest
 def largestPerimeter(nums: List[int]) -> int:
     nums.sort(reverse=True)
 
-    if len(nums) > 2 and nums[0] >= nums[1] + nums[2]:
+    while len(nums) > 2 and nums[0] >= nums[1] + nums[2]:
         nums.pop(0)
 
     return 0 if len(nums) < 3 else sum(nums[:3])
