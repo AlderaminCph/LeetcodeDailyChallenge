0fa80d438cba88082d5d4518d2d133026bf3df9f
AlderaminCph <sypkova.anastasia@yandex.ru>
2022-10-13 17:02:04 +0300

    remove elements from list problem solving started

---
+++ problems/remove_linked_list_elements.py
@@ -0,0 +1,42 @@
+"""
+Given the head of a linked list and an integer val, remove all the nodes of
+the linked list that has Node.val == val, and return the new head.
+
+
+
+Example 1:
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+"""
+from typing import Optional
+
+
+class ListNode:
+    # Definition for singly-linked list.
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(
+        self, head: Optional[ListNode], val: int
+    ) -> Optional[ListNode]:
+        current_node = head
+        while current_node:
+            if current_node.val == val:
+                current_node.val = current_node.next.val
+                current_node.next = current_node.next.next
+            current_node = current_node.next
+        return current_node
